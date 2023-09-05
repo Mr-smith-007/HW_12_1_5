3a1dbb264fa0454581efa0ba900fddfb10aaa402
Repnikov.A <repnikov.a@florcom.ru>
2023-09-05 14:30:52 +0300

    На проверку

--- HW_12_1_5/Program.cs
+++ HW_12_1_5/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+
+namespace HW_12_1_5
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            users.Add(new UserInfo("Tom123", "Tom", true));
+            users.Add(new UserInfo("MiKe_005", "Mike", false));
+            users.Add(new UserInfo("Alex__O", "Alex", false));
+            users.Add(new UserInfo("AgataSnow", "Agata", true));
+            users.Add(new UserInfo("Mr_smith", "Nick", true));
+
+            foreach (UserInfo user in users)
+            {
+                Console.WriteLine($"Hello, {user.Name}!");
+                if (!user.IsPremium)
+                    ShowAdvertising.ShowAds();
+                Console.WriteLine();
+            }
+
+        }
+
+        
+    }
+}
---
+++ HW_12_1_5/UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_12_1_5
+{
+    internal class UserInfo
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public UserInfo(string login, string name, bool prem) 
+        {
+            Login = login;
+            Name = name;    
+            IsPremium = prem;
+        }
+    }
+}
